b35ca743fbffff9bb1b2aa877ca4750dbdd35fe5
serge_neri <saneri@uncc.edu>
2015-11-07 03:16:28 +0000

    Finished Ch4 Walk Through.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,13 +3,40 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-  def new
-  # default: render 'new' template
+  
+  # in app/controllers/movies_controller.rb
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
   end
+  
+  def new
+    # default: render 'new' template
+  end
+  
   # in movies_controller.rb
   def create
     @movie = Movie.create! movie_params
     flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  
+  def edit
+    @movie = Movie.find params[:id]
+  end
+ 
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes! movie_params
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
+  def destroy
+  @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:notice] = "#{@movie.title} was successfully deleted."
     redirect_to movies_path
   end
   
@@ -19,3 +46,4 @@ class MoviesController < ApplicationController
   end
 
 end
+
